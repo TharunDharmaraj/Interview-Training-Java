3ba1ff79376d4c7c507152c4fd80ed91b821c67b
Tharun <tharundharmaraj@gmail.com>
2023-03-03 20:36:46 +0530

    day3

--- Find Duplicate/hashMap.java
+++ Find Duplicate/hashMap.java
@@ -11,3 +11,16 @@ class Solution {
        return 0;
     }
 }
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//             if(map.get(nums[i])==2){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
