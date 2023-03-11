5b43acc77476e4eb40d03efc107b36147471f61e
Tharun <tharundharmaraj@gmail.com>
2023-03-11 16:54:06 +0530

    day8

--- Merge sorted array/naive.java
+++ Merge sorted array/naive.java
@@ -1,11 +1,11 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index1 = 0, index2 = 0, k=0;
+        int j=0;
         for(int i = m ; i < m + n ; i++){
-            nums1[i] = nums2[k++];
+            nums1[i] = nums2[j++];
         }
         for(int i = 0 ; i < m+n ; i++){
-            for(int j = m+n-1 ; j > i ; j--){
+            for(j = m+n-1 ; j > i ; j--){
                 if(nums1[j] < nums1[j - 1]){
                     int temp = nums1[j];
                     nums1[j] = nums1[j - 1];
