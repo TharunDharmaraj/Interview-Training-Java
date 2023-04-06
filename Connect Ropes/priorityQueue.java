09ea530db70fb2b942f8317a44570773f496032c
Tharun <tharundharmaraj@gmail.com>
2023-04-07 00:33:14 +0530

    day 12

---
+++ Connect Ropes/priorityQueue.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public static int connectRopes(int[] lengths, int n) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i : lengths) {
+            pq.add(i);
+        }
+        int minLength = 0, first = 0, second = 0;
+        while (pq.size() > 1) {
+            first = pq.peek();
+            pq.poll();
+            second = pq.peek();
+            pq.poll();
+            int min = first + second;
+            minLength = minLength + min;
+            pq.add(min);
+        }
+        return minLength;
+    }
+}
