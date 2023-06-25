38cad1c96850173e853796c218a9f3735a688f60
Tharun <83175935+TharunDharmaraj@users.noreply.github.com>
2023-06-25 18:53:01 +0530

    day23

---
+++ Reverse_alphaNum/ans.java
@@ -0,0 +1,41 @@
+package Reverse_alphaNum;
+
+import java.util.Scanner;
+
+// Given a sentence containing many words you have to reverse all the alphanumeric characters in each and every word of the sentence.
+// Input:  "my ^&na$me@#$ is123 Mike"
+// Output: "ym ^&an$em@#$ 321si ekiM"
+
+class ans {
+    public static void reverse(String str) {
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.print(rev);
+    }
+
+    public static void main(String[] args) {
+        // Scanner sc =new Scanner(System.in);
+        String str = "my na$me@#$ is123 Mike@";
+        int start = 0, end = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                end++;
+            } else {
+                if (start != end) {
+                    reverse(str.substring(start, end));
+                                        System.out.print(ch);
+
+                } else {
+                    System.out.print(ch);
+                }
+                start = i + 1;
+                end = i + 1;
+            }
+        }
+        System.out.println();
+        System.out.println(str);
+    }
+}
