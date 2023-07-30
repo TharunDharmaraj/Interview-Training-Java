e500cacb0e87dc28672e70c364863fcb1a97f1d9
TharunDharmaraj <tharundharmaraj@gmail.com>
2023-07-30 19:01:25 +0530

    day35

--- remove Duplicate From Sorted Array II/Solution.java
+++ remove Duplicate From Sorted Array II/Solution.java
@@ -1,6 +1,6 @@
 package remove Duplicate From Sorted Array II;
 // Link : https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/solutions/3741215/simple-two-pointer-approach-tc-o-n/
-public class Solution {
+class Solution {
     public int removeDuplicates(int[] nums) {
         int len = nums.length;
         if(len <= 2){
@@ -13,6 +13,13 @@ public class Solution {
             }
             nums[++rIndex] = nums[i];
         }
+        // int k = 0;
+        // for(int j : nums){
+        //     if(k <= rIndex + 1){
+        //         System.out.print(j + " ");
+        //         k++;
+        //     }
+        // }
         return rIndex + 1;
     }
 }
