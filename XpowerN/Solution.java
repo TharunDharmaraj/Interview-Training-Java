f1cf9f065e8aa2ca519cde3cb6937a68d6421075
TharunDharmaraj <tharundharmaraj@gmail.com>
2023-07-30 20:48:20 +0530

    day35

--- XpowerN/Solution.java
+++ XpowerN/Solution.java
@@ -1,26 +1,23 @@
 // Link: https://leetcode.com/problems/powx-n/solutions/3807721/2-optimised-method-bit-recursion-video-explanation-faster-log-n-solution/
 package XpowerN;
-
 class Solution {
-    public double solve(double x, long n) {
-        if (n == 0) {
+    public double solve(double x , int n){
+        if(n == 0){
             return 1;
         }
-        double temp = solve(x, n / 2);
+        double temp = solve(x,n/2);
         temp = temp * temp;
-        if (n % 2 == 0) {
+        if(n % 2 == 0){
             return temp;
-        } else {
+        }else{
             return temp * x;
         }
     }
-
     public double myPow(double x, int n) {
-        long num = n;
-        double ans = solve(x, num);
-        if (n < 0) {
-            return 1 / ans;
-        } else {
+        double ans = solve(x,n);
+        if(n < 0){
+            return 1/ans;
+        }else{
             return ans;
         }
     }
